bc81280ca9a918d5661d13c6f7b8d5570ca10af8
SHB <andyshb95@gmail.com>
2023-05-09 15:06:05 -0400

    Finished Element Position

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('Script Started');
+
+const cardNumberShow = document.querySelector('.cardNumber');
+const cardNameShow = document.querySelector('.cardName');
+const cardDateShow = document.querySelector('.cardDate');
+const cardCVCShow = document.querySelector('.cardBack');
+
+
+cardNumberShow.innerHTML = '1234 5678 9123 4567';
+cardNameShow.innerHTML = 'Andy Perera';
+cardDateShow.innerHTML = '10/24';
+
+function transformCardNumber(number){
+    
+}
